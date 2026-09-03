db112a5a9766f1ae6b8380e78fbb43121046b74f
Ali N. Johnson <alin.johnson@Mac.router>
2026-09-03 15:35:08 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.scss";
 export const metadata: Metadata = {
   title: "the baking fairy report",
   description: "a journey of flour + fairy dust",
+  icons: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🧚🏼</text></svg>",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
